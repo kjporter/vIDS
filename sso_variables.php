5d4b693f4455cca81f10208728ffe346fd05ab13
Kyle <kporter@kplink.net>
2021-08-30 20:57:01 -0700

    Updated SSO module
    Transitioned VATSIM SSO code module to a class

--- sso_variables.php
+++ sso_variables.php
@@ -9,17 +9,17 @@ $client_id = "aaa";
 $client_secret = "aaa";
 $redirect_uri = "http://aaa";
 $sso_endpoint = "https://auth-dev.vatsim.net";
-$sso_variables[] = array($client_id,$client_secret,$redirect_uri,$sso_endpoint);
+$sso_variables[] = array('client_id'=>$client_id,'client_secret'=>$client_secret,'redirect_uri'=>$redirect_uri,'sso_endpoint'=>$sso_endpoint);
 // [1] For use on BBB's server
 $client_id = "bbb";
 $client_secret = "bbb";
 $redirect_uri = "https://bbb";
 $sso_endpoint = "https://auth-dev.vatsim.net";
-$sso_variables[] = array($client_id,$client_secret,$redirect_uri,$sso_endpoint);
+$sso_variables[] = array('client_id'=>$client_id,'client_secret'=>$client_secret,'redirect_uri'=>$redirect_uri,'sso_endpoint'=>$sso_endpoint);
 // [2] For use on live CCC ARTCC server 
 $client_id = "ccc";
 $client_secret = "ccc";
 $redirect_uri = "https://ccc";
 $sso_endpoint = "https://auth.vatsim.net";
-$sso_variables[] = array($client_id,$client_secret,$redirect_uri,$sso_endpoint);
+$sso_variables[] = array('client_id'=>$client_id,'client_secret'=>$client_secret,'redirect_uri'=>$redirect_uri,'sso_endpoint'=>$sso_endpoint);
 ?>
